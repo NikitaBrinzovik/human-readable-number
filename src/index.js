ce9e54f88b49b7cc3a4f179f023da7d905c4ab3c
Nikita Kuznetsov <75593249+NikitaBrinzovik@users.noreply.github.com>
2023-02-09 18:00:05 +0300

    fix: remove garbage

--- src/index.js
+++ src/index.js
@@ -3,24 +3,20 @@ const numbers = {
     tens: ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
 }
 
-module.exports = function toReadable (num) {
+module.exports = function toReadable(num) {
     if (!num || num === 0) return 'zero';
 
     let result = '';
-
-    while (num > 0) {
-        const oneThousandthPart = num % 1000;
-        if (oneThousandthPart) {
-            const currentPowers = Math.floor(oneThousandthPart / 100);
-            const currentTens = Math.floor((oneThousandthPart % 100) / 10);
-            const currentOnes = oneThousandthPart % 10;
-            result =
-                (currentPowers ? numbers.ones[currentPowers] + ' hundred ' : '')
-                + (currentTens >= 2
-                    ? numbers.tens[currentTens] + ' ' + numbers.ones[currentOnes]
-                    : numbers.ones[currentTens * 10 + currentOnes]);
-        }
-        num = Math.floor(num / 1000);
+    const oneThousandthPart = num % 1000;
+    if (oneThousandthPart) {
+        const currentPowers = Math.floor(oneThousandthPart / 100);
+        const currentTens = Math.floor((oneThousandthPart % 100) / 10);
+        const currentOnes = oneThousandthPart % 10;
+        result =
+            (currentPowers ? numbers.ones[currentPowers] + ' hundred ' : '')
+            + (currentTens >= 2
+                ? numbers.tens[currentTens] + ' ' + numbers.ones[currentOnes]
+                : numbers.ones[currentTens * 10 + currentOnes]);
     }
 
     return result.trim();
