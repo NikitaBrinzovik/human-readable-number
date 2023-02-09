9c50bbebef3a520193c28eb27ebf5c9a3c6f21a1
Nikita Kuznetsov <75593249+NikitaBrinzovik@users.noreply.github.com>
2023-02-09 17:42:20 +0300

    init: function for converting numbers to text format

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
+const numbers = {
+    ones: ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+    tens: ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
 }
+
+module.exports = function toReadable (num) {
+    if (!num || num === 0) return 'zero';
+
+    let result = '';
+
+    while (num > 0) {
+        const oneThousandthPart = num % 1000;
+        if (oneThousandthPart) {
+            const currentPowers = Math.floor(oneThousandthPart / 100);
+            const currentTens = Math.floor((oneThousandthPart % 100) / 10);
+            const currentOnes = oneThousandthPart % 10;
+            result =
+                (currentPowers ? numbers.ones[currentPowers] + ' hundred ' : '')
+                + (currentTens >= 2
+                    ? numbers.tens[currentTens] + ' ' + numbers.ones[currentOnes]
+                    : numbers.ones[currentTens * 10 + currentOnes]);
+        }
+        num = Math.floor(num / 1000);
+    }
+
+    return result.trim();
+}
+
